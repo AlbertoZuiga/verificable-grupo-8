fb45b9404f71532bc2055c090724306a2308909f
Tomaspoch1 <tpoch@miuandes.cl>
2025-05-09 10:56:15 -0400

    Ready evaluation instance

--- app/services/evaluation_instance_service.py
+++ app/services/evaluation_instance_service.py
@@ -1,4 +1,6 @@
+from app import kanvas_db
 from app.models import EvaluationInstance, StudentSection, StudentEvaluationInstance
+
 
 def get_evaluation_instance_with_students_and_grades(evaluation_instance_id):
     evaluation_instance = EvaluationInstance.query.get_or_404(evaluation_instance_id)
@@ -15,3 +17,32 @@ def get_evaluation_instance_with_students_and_grades(evaluation_instance_id):
     }
 
     return evaluation_instance, students, student_grades
+
+
+def get_evaluation_instance_and_student(evaluation_instance_id, student_id):
+    evaluation_instance = EvaluationInstance.query.get_or_404(evaluation_instance_id)
+    student = next((s for s in evaluation_instance.evaluation.section.students if s.id == student_id), None)
+    return evaluation_instance, student
+
+
+def get_student_grade_instance(evaluation_instance_id, student_id):
+    return StudentEvaluationInstance.query.filter_by(
+        evaluation_instance_id=evaluation_instance_id,
+        student_id=student_id
+    ).first()
+
+
+def save_student_grade(evaluation_instance_id, student_id, grade_value):
+    student_grade = get_student_grade_instance(evaluation_instance_id, student_id)
+
+    if student_grade:
+        student_grade.grade = grade_value
+    else:
+        student_grade = StudentEvaluationInstance(
+            student_id=student_id,
+            evaluation_instance_id=evaluation_instance_id,
+            grade=grade_value
+        )
+        kanvas_db.session.add(student_grade)
+
+    kanvas_db.session.commit()
