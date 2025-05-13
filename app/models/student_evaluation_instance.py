7ae4d2d684d679f08f782001837ef68a4c71a6f3
Alberto Zúñiga <albertozuniga@Albertos-MacBook-Pro.local>
2025-05-13 13:01:16 -0400

    hot fix model student_evaluation_instance

--- app/models/student_evaluation_instance.py
+++ app/models/student_evaluation_instance.py
@@ -5,13 +5,11 @@ class StudentEvaluationInstance(kanvas_db.Model):
 
     student_id = kanvas_db.Column(kanvas_db.Integer, kanvas_db.ForeignKey('students.id'), primary_key=True)
     evaluation_instance_id = kanvas_db.Column(kanvas_db.Integer, kanvas_db.ForeignKey('evaluation_instances.id'), primary_key=True)
-    evaluation_id = kanvas_db.Column(kanvas_db.Integer, kanvas_db.ForeignKey('evaluations.id'), primary_key=True)
 
     grade = kanvas_db.Column(kanvas_db.Float)
 
     student = kanvas_db.relationship('Student', back_populates='student_evaluation_instances')
     evaluation_instance = kanvas_db.relationship('EvaluationInstance', back_populates='student_evaluation_instances')
-    evaluation = kanvas_db.relationship('Evaluation')  
 
     def __repr__(self):
-        return f"<StudentEvaluationInstance student_id={self.student_id} evaluation_instance_id={self.evaluation_instance_id} evaluation_id={self.evaluation_id} grade={self.grade}>"
+        return f"<StudentEvaluationInstance student_id={self.student_id} evaluation_instance_id={self.evaluation_instance_id} grade={self.grade}>"
