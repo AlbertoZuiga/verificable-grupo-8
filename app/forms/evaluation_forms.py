be74721a01a5508191d6f10fc8fcc1142b1ed6e0
Alberto Zúñiga <albertozuniga@Albertos-MacBook-Pro.local>
2025-06-17 11:00:35 -0400

    evaluation form

---
+++ app/forms/evaluation_forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+from app.models.section import WeighingType
+
+class EvaluationForm(FlaskForm):
+    section_id = SelectField("Sección", validators=[DataRequired()], coerce=int)
+    title = StringField("Título de la Evaluación", validators=[DataRequired(), Length(max=60)])
+    weighing_system = SelectField(
+        "Sistema de Ponderación",
+        choices = [(type.name, type.value) for type in WeighingType],
+        validators=[DataRequired()]
+    )
+    submit = SubmitField("💾 Guardar Cambios")
