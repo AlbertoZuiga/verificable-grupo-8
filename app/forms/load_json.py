7304ae098558b27d2f817e8ad80b737be44198ca
Alberto Zúñiga <albertozuniga@Albertos-MacBook-Pro.local>
2025-06-17 12:08:40 -0400

    validate json input

--- app/forms/load_json.py
+++ app/forms/load_json.py
@@ -1,13 +1,45 @@
+import json
+from flask import request
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import HiddenField, SubmitField, ValidationError
 
 from app.utils import json_constants as JC
 
+MAX_FILE_SIZE = 2 * 1024 * 1024
+
+def validate_file_size(form, field):
+    if request.content_length > MAX_FILE_SIZE:
+        raise ValidationError('El archivo excede el tamaño máximo permitido (2MB).')
+
+def validate_file(form, field):
+    file_data = field.data
+    try:
+        file_bytes = file_data.read()
+        file_data.seek(0)
+
+        try:
+            file_contents = file_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            raise ValidationError('El archivo no está codificado en UTF-8 o no es texto válido.')
+
+        data = json.loads(file_contents)
+
+        if form.json_type.data not in UploadJSONForm.allowed_types:
+            raise ValidationError('Tipo de JSON inválido.')
+
+        if not isinstance(data, dict):
+            raise ValidationError('El archivo JSON debe tener un objeto como estructura principal.')
+
+    except json.JSONDecodeError:
+        raise ValidationError('El contenido del archivo no es un JSON válido.')
+
 class UploadJSONForm(FlaskForm):
     file = FileField('Selecciona un archivo JSON:', validators=[
         FileRequired(),
-        FileAllowed(['json'], 'Solo archivos JSON permitidos.')
+        FileAllowed(['json'], 'Solo archivos JSON permitidos.'),
+        validate_file_size,
+        validate_file
     ])
     json_type = HiddenField()
     submit = SubmitField('Subir')
